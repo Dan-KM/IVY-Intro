a91fabf2a5577b9613d16db1cf375563c8fefa19
Dan-KM <danmulumbi8@gmail.com>
2025-02-21 02:46:17 +0300

    Initiation of all the pages needed

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import Navbar from './components/NavBar.vue';
+  import FooterComponent from './components/Footer.vue';
+</script>
+
+<template>
+  <header>
+      <nav>
+        <Navbar />
+      </nav>
+  </header>
+
+  <RouterView class = "mt-[5%]"/>
+
+  <FooterComponent />
+</template>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutUsView from '../views/AboutUsView.vue'
+import ContactUsView from '@/views/contactUsView.vue'
+import VaccinationDetails from '@/views/vaccinationDetails.vue'
+import ClinicsView from '@/views/clinicsView.vue'
+import FAQView from '@/views/FAQView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about-us',
+      name: 'about-us',
+      component: AboutUsView,
+    },
+    {
+      path: '/contact-us',
+      name: 'contact-us',
+      component: ContactUsView,
+    },
+    {
+      path: '/vaccinations',
+      name: 'vaccinations',
+      component: VaccinationDetails,
+    },
+    {
+      path: '/clinics',
+      name: 'Clinics near you',
+      component: ClinicsView,
+    },
+    {
+      path: '/questions',
+      name: 'Questions',
+      component: FAQView,
+    },
+  ],
+})
+
+export default router
